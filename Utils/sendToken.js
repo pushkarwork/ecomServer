7549741cc89103656867ac0ac66af4ce3501927f
Pushkar Verma <pushkar753@gmail.com>
2024-08-27 16:15:31 +0530

    Update sendToken.js

--- Utils/sendToken.js
+++ Utils/sendToken.js
@@ -1,15 +1,15 @@
 module.exports = (user, statusCode, res) => {
     const token = user.generateJwtToken()
 
-    const options = {
+   const options = {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE_TIME * 24 * 60 * 60 * 1000
         ),
-        httpOnly: false,
-        secure: process.env.NODE_ENV === 'PRODUCTION', 
-        sameSite: 'None'
-        // secure: false
-    }
+        httpOnly: true,
+        secure: true, // Enable this if your site is served over HTTPS
+        sameSite: 'None', // Allow cross-origin cookies
+        domain: '.onrender.com' // Set domain for cross-subdomain cookies
+    };
     console.log("here in sendTOken",token)
     res.status(statusCode).json({ token })
        
